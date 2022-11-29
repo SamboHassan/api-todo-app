867702131f9671489c817d627df2f31fbbd8eb8c
Hassan Sambo <sambohasan@gmail.com>
2022-11-29 15:39:19 +0100

    feat: add statics files

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const todoRoutes = require('./routes/todos')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); //allow us to access req body
-app.use('/api/todos', todoRoutes)
+app.use('/api/todos', todoRoutes);
+app.use(express.static(__dirname + 'public'))
+app.use(express.static(__dirname + '/views'));
+
+// app.get('/', function(req, res){
+//     res.send("HELLO FROM ROOT ROUTE");
+// })
 
 app.get('/', function(req, res){
-    res.send("HELLO FROM ROOT ROUTE");
+    res.sendFile("index.html");
 })
 
 
